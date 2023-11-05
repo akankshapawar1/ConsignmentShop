67664a00a1adad99f8be0d4f838cd77688b2ca29
Vignesh Sundaram <vigneshs1006@gmail.com>
2023-11-05 11:19:26 -0500

    Merge changes

--- consignment-shop/src/App.js
+++ consignment-shop/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import Login from "./login";
-import StoreOwner from "./StoreOwner";
+import StoreOwner from "./storeowner";
 import SiteManager from './SiteManager';
 
 const styles = {
